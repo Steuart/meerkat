3c3adee0abeaeddd3dd3fe65199f066152d8d20b
Ming <jianmo_o@163.com>
2023-08-06 23:53:05 +0800

    design trade record table structure

---
+++ src/main/java/cc/jooylife/meerkat/repository/mapper/TradeRecordMapper.java
@@ -0,0 +1,15 @@
+package cc.jooylife.meerkat.repository.mapper;
+
+/**
+* @author Ming
+* @description 针对表【trade_record】的数据库操作Mapper
+* @createDate 2023-08-06 23:50:18
+* @Entity cc.jooylife.meerkat.repository.entity.TradeRecord
+*/
+public interface TradeRecordMapper {
+
+}
+
+
+
+
